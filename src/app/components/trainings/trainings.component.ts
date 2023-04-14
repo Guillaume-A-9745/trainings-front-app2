c22554dd2a092805c5d82a094e80d763dd0f7bdb
Anselme Guillaume <anselmeguillaume@orange.fr>
2023-04-14 14:53:28 +0200

    ajout footer

--- src/app/components/trainings/trainings.component.ts
+++ src/app/components/trainings/trainings.component.ts
@@ -17,7 +17,11 @@ export class TrainingsComponent implements OnInit {
       { id:1, name:'Java', description:'Formation Java SE 8 sur 5 jours', price:1500, quantity:1 },
       { id:2, name:'Java Avancée', description:'Formation Java Avancée sur 5 jours', price:1500, quantity:1 },
       { id:3, name:'DotNet', description:'Formation DotNet sur 3 jours', price:1000, quantity:1 },
-      { id:4, name:'Python', description:'Formation Python/Django sur 5 jours', price:1500, quantity:1 }
+      { id:4, name:'Python', description:'Formation Python/Django sur 5 jours', price:1500, quantity:1 },
+      { id:5, name:'Spring', description:'Spring Core/Mvc/Security sur 5 jours', price:1799, quantity:1 },
+      { id:6, name:'Php frameworks', description:'Formation Symphony sur 5 jours', price:1200, quantity:1 },
+      { id:7, name:'VPN et Firewall', description:'Proteger vos reseaux des tentatives d intrusion sur 5 jours', price:1500, quantity:1 },
+      { id:8, name:'Google analytics', description:'collecter et analyser les donniees utilisateurs sur 5 jours', price:3500, quantity:1 }
     ]
   }
 
